61d05c5f32fec98281d605b9b77c6dbed6271510
goiabae <goiabae@github.com>
2021-08-25 01:21:17 -0300

    started implementing tree structure

--- src/main.c
+++ src/main.c
@@ -2,7 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 
-/* NOTE every time we call this function with the same file pointer
+/* declare a node type for our graph */
+typedef struct node Node;
+/* and then define it */
+struct node {
+    int value;
+    char letter;
+    /* this is called a "self-referencing" struct,
+     * which has some influence from OOP (not really) */
+    Node* child;
+    Node* sibling;
+};
+
+/* NOTE: every time we call this function with the same file pointer
  *we continue in the previous line that we were, so that there is no need
  *to return it to the start */
 
@@ -16,10 +28,10 @@ int get_size(FILE* file) {
 }
 
 
-/* NOTE this function consumes a lot of memory because of all those mallocs */
+/* NOTE: this function consumes a lot of memory because of all those mallocs */
  /* receives: file pointer, pointer to an empty array of characters, size of the set
   * returns: integer (1 = end of file/ error  0 = sucess)
-  * HACK mutates the array from the pointer */
+  * HACK: mutates the array from the pointer */
 int get_set(FILE* file, char** data_arr, int size) {
     int i;
 
@@ -28,9 +40,9 @@ int get_set(FILE* file, char** data_arr, int size) {
    if (!feof(file)) {
        for (i = 0; i < size; i++) {
         /* we allocate a 50 character long string for each array element */
-        data_arr[i] = malloc(sizeof(char) * 50);
+        data_arr[i] = malloc(sizeof(char) * 80);
         /* and then get the next line of the file and put it into that empty string */
-        fgets(data_arr[i], 50, file);
+        fgets(data_arr[i], 80, file);
        }
    } else {
        return 1;
@@ -39,13 +51,48 @@ int get_set(FILE* file, char** data_arr, int size) {
     return 0;
 }
 
+/* receives: letter of the correspondent character
+ * returns: a new Node struct */
+Node* new_node(char letter) {
+    Node* result = malloc(sizeof(Node));
+    if (result != NULL) {
+        result->value = 1;
+        result->child = NULL;
+        result->sibling = NULL;
+        result->letter = letter;
+    }
+    return result;
+}
+
+/* generates a new branch on the tree depending on the existing one */
+Node* gen_branch(char string[], Node* previous, int pos) {
+    if ( &previous->letter != NULL ) {
+        previous->child = new_node(string[pos+1]);
+    }
+
+/* receives: dataset (array of strings), number of strings in dataset
+ * returns: void (NOTE: not decided yet)
+ * HACK: generates the tree structure on the heap (NOTE: has to be freed manually later) */
+void gen_tree(char** dataset, int size) {
+    for (int i = 0; i < size; i++) {
+        Node* branch = new_node(dataset[i][0]);
+        gen_branch(dataset[i], branch, 1);
+    }
+}
+
+float get_score(char** dataset, int size) {
+    float average = 0;
+
+    gen_tree(dataset, size);
+    return average;
+}
+
 int main(int argc, char* argv[]) {
    FILE* file;
    char* filename;
    char** dataset;
    int size;
    /* int i;  iterator index */
-
 
    if (argc != 2) {
         printf("Incorrect number of arguments\nCorrect use:\tshorthand [file path]\n");
@@ -66,7 +113,7 @@ int main(int argc, char* argv[]) {
    dataset = malloc(size * sizeof(char*));
    get_set(file, dataset, size);
 
-   /* gen_graph(dataset); TODO -> needs implementation */
+   /* get_score(dataset, size); TODO -> needs implementation */
 
    fclose(file);
    /* FILE SCOPE END */
